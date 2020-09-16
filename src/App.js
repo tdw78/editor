ebea87dc304e7897746fbb49e53e71183bb8516c
Tim Weiss <tdweiss1978@gmail.com>
2020-09-16 10:30:03 -0700

    Basic setup complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import Landing from './components/Landing';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Text Editor
-      </header>
+      <Landing />
     </div>
   );
 }
---
+++ src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Landing() {
+  return (
+    <div>
+      <header className="main-header">
+        Text Editor
+      </header>
+      <p>Input box goes here</p>
+    </div>
+  );
+}
+
+export default Landing;
